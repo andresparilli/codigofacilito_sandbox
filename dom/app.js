aac9d11d67661ccf31876bf774f375b7dbd36a63
Andres Parilli <andres.parilli@gmail.com>
2023-11-13 19:53:32 -0400

    items

--- dom/app.js
+++ dom/app.js
@@ -1,2 +1,5 @@
 const title = document.getElementById('title');
 title.innerHTML = "Cursos";
+
+const description = document.getElementById('description');
+description.innerHTML = "Listado de cursos"
