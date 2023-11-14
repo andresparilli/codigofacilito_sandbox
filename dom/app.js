b76a5c5f15035cc999ae304f295e0bda9bfd0b44
Andres Parilli <andres.parilli@gmail.com>
2023-11-13 20:01:07 -0400

    Elemntos por clase

--- dom/app.js
+++ dom/app.js
@@ -3,3 +3,9 @@ title.innerHTML = "Cursos";
 
 const description = document.getElementById('description');
 description.innerHTML = "Listado de cursos"
+
+const items = document.getElementsByClassName("list-group-item");
+for (var i=0; i < items.length; i++){
+    let element = items[i];
+    console.log(element);
+}
