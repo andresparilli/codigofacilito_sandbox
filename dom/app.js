6e17250cefe99cbd1adda401c4ae723eadb052e8
Andres Parilli <andres.parilli@gmail.com>
2023-11-13 22:41:04 -0400

    previene el compartmineto, e imprime los valores

--- dom/app.js
+++ dom/app.js
@@ -13,4 +13,14 @@ for (var i=0; i < items.length; i++){
 }
 
 const form = document.getElementById('course-form');
-console.log(form);
+
+form.addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    let title = document.getElementById('title-form').value;
+    let description = document.getElementById('description-form').value;
+
+    console.log(title);
+    console.log(description);
+});
