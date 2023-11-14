1400b0d9bf597651c8c748ca8c59fb341232500f
Andres Parilli <andres.parilli@gmail.com>
2023-11-13 23:44:27 -0400

    Crear nuevo elemnto

--- dom/app.js
+++ dom/app.js
@@ -14,8 +14,11 @@ document.addEventListener('DOMContentLoaded', function(){
         for (var i=0; i < items.length; i++){
             let element = items[i];
             element.style.background = '#f2f2f2'
-        }
+        };
+    
+    const row = document.querySelector('.row');
 
+    
     const form = document.getElementById('course-form');
 
 //trabajar el submit en el formulario y no en el boton
@@ -27,12 +30,21 @@ document.addEventListener('DOMContentLoaded', function(){
         let title = document.getElementById('title-form').value;
         let description = document.getElementById('description-form').value;
 
-        console.log(title);
-        console.log(description);
+        create_card(title, description);
     });
 
+    function create_card(title, description){
+        let html = `<div class="col-sm-6 col-md-4">
+                        <div calss="thumbnail">
+                            <div class="caption">
+                                <h3 id="title_card"> ${title} </h3>
+                                <p id="description_card"> ${description} </p>
+                                <p> <a href="#" class="btn btn-danger">Acción</a></p>
+                            </div>
+                        </div>
+                    </div>`;
+        row.innerHTML += html;
+
+    }
+
 });
-
-/*
-
-*/
