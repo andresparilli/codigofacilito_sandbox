587ee92e55846f047ccb902d4e3bb3ccad5dc744
Andres Parilli <andres.parilli@gmail.com>
2023-11-13 23:10:22 -0400

    fin de clase 22

--- dom/app.js
+++ dom/app.js
@@ -1,28 +1,38 @@
-const title = document.getElementById('title');
-title.innerHTML = "Cursos";
+document.addEventListener('DOMContentLoaded', function(){
 
-const description = document.getElementById('description');
-description.innerHTML = "Listado de cursos"
+    console.log("El dom ya fue construido");
+
+    const title = document.getElementById('title');
+        title.innerHTML = "Cursos";
+
+    const description = document.getElementById('description');
+        description.innerHTML = "Listado de cursos"
 
 //const items = document.getElementsByClassName("list-group-item");
 
-const items = document.querySelectorAll("li:nth-child(even)");
-for (var i=0; i < items.length; i++){
-        let element = items[i];
-        element.style.background = '#f2f2f2'
-}
+    const items = document.querySelectorAll("li:nth-child(even)");
+        for (var i=0; i < items.length; i++){
+            let element = items[i];
+            element.style.background = '#f2f2f2'
+        }
 
-const form = document.getElementById('course-form');
+    const form = document.getElementById('course-form');
 
 //trabajar el submit en el formulario y no en el boton
-form.addEventListener('submit', function(e){
+    form.addEventListener('submit', function(e){
 
     //recordar este metodo, para prevenir el envio
-    e.preventDefault();
+        e.preventDefault();
 
-    let title = document.getElementById('title-form').value;
-    let description = document.getElementById('description-form').value;
+        let title = document.getElementById('title-form').value;
+        let description = document.getElementById('description-form').value;
 
-    console.log(title);
-    console.log(description);
+        console.log(title);
+        console.log(description);
+    });
+
 });
+
+/*
+
+*/
